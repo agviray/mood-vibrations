39143a30d2ce5241360d693199d2403b52358793
Allan Viray <allang.v.dev@gmail.com>
2025-01-24 18:53:05 -0600

    add initial form to take in and submit user mood

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [userMood, setUserMood] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <h1>Mood Vibrations</h1>
+      <div id="user-input">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={userMood}
+            onChange={(event) => setUserMood(event.target.value)}
+          />
+          <button type="submit">Submit Mood</button>
+        </form>
+      </div>
+      <div id="recommendation">
+        {/* Display music/sound recommendation here */}
+      </div>
     </>
   );
 };
