63d340ce614ad5841d6b9b6c40cb53a2bc30304d
Allan Viray <allang.v.dev@gmail.com>
2025-03-18 19:38:05 -0500

    relocate openai setup to  server/config and refactor prompt controller

--- server/controllers/api/prompt.js
+++ server/controllers/api/prompt.js
@@ -1,34 +1,16 @@
-const OpenAI = require('openai');
+const { analyze } = require('../../config/openai');
 
-module.exports = {
-  analyze,
-};
-
-async function analyze(req, res) {
-  const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
+const analyzeMood = async (req, res) => {
   const { userPrompt } = req.body;
 
   try {
-    const firstResponse = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      prompt: `The user will provide some text to you. Analyze the text to determine the user's overall emotional state. Consider factors such as word choice, sentence structure, and sentiment expressed. Respond by acknowledging the user's possible mood that you determined, without repeating their text, in a mimimum of one to a maximum of two complete sentences. The last sentence must be a grammatically complete sentence. Don't refer to the user as "the user" or "they." Instead, refer to the user as "you." Do not include the basis of your analysis or determination. For example, do not say things like "According to your text" or "Based on your text...". Sound more human rather than robotic. Here is the text to analyze: "${userPrompt}"`,
-      max_tokens: 50,
-    });
-
-    const moodMessage = firstResponse.choices[0].text.trim();
-
-    const secondResponse = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      prompt: `I will provide some text to you. Analyze the text to determine an overall emotional state. Respond in a single word that describes the mood that the text conveys. The text is: "${userPrompt}"`,
-      max_tokens: 40,
-    });
-
-    const mood = secondResponse.choices[0].text.trim();
-
+    const { moodMessage, mood } = await analyze(userPrompt);
     res.json({ moodMessage, mood });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
+
+module.exports = {
+  analyzeMood,
+};
--- server/routes/api/prompt.js
+++ server/routes/api/prompt.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const promptCtrl = require('../../controllers/api/prompt');
 
-router.post('/analyze', promptCtrl.analyze);
+router.post('/analyze', promptCtrl.analyzeMood);
 
 module.exports = router;
