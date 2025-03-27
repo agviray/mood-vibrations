b423886110e3b9e661c56a3605e5433c0db94fc6
Allan Viray <allang.v.dev@gmail.com>
2025-03-26 20:12:00 -0500

    create initial PlaylistCard component

---
+++ client/src/components/PlaylistCard.jsx
@@ -0,0 +1,14 @@
+const PlaylistCard = () => {
+  return (
+    <div className="StyledPlaylistCard">
+      <img src="" alt="Playlist Image" />
+      <h4>Playlist Name</h4>
+      <p>Playlist Description</p>
+      <a href="" target="_blank">
+        <span className="playlist-link">External link to playlist</span>
+      </a>
+    </div>
+  );
+};
+
+export default PlaylistCard;
